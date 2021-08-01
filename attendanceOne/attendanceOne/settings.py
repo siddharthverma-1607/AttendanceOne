fde196ae97917e524eb87ea680face5b5ffd0a96
Siddharth Verma <ashardeesidd@gmail.com>
2021-08-01 16:53:22 +0530

    changes to settings.py

--- attendanceOne/attendanceOne/settings.py
+++ attendanceOne/attendanceOne/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import django_heroku
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
